87bbce79aa91e44b60165e2c06b7227fefef5705
Avisek Kar <104550345+avisek-official@users.noreply.github.com>
2022-09-14 11:20:19 +0530

    Update api.js

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,7 +1,7 @@
 export const cityAPIOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "26451fa60dmshcb3fad749b300fap115f0fjsn20ac664d02e1",
+    "X-RapidAPI-Key": [API_KEY],
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,4 +9,4 @@ export const cityAPIOptions = {
 export const CityAPI_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WeatherAPI_URL = "https://api.openweathermap.org/data/2.5";
-export const WeatherAPI_KEY = "da5928c1ce532df32d84e52578f2f12f";
+export const WeatherAPI_KEY = [API_KEY];
